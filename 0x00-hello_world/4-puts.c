4beade2609391503ef92ebe587f8c3f02cafcc7b
besthor <igbebestor7@gmail.com>
2022-09-08 13:37:35 -0700

    a C program that prints exactly (Programming is like building a multilingual puzzle) followed by a new line.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
