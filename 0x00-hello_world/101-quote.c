57055df37e85579985d3d94a7cc431b620673fbc
besthor <igbebestor7@gmail.com>
2022-09-08 19:02:04 -0700

    a C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,16 +2,15 @@
 #include <unistd.h>
 
 /**
- * main - print the string in the out function
+ * main - print the string in the put function
+ *
  * Description: using the main function
  * this program prints "and that piece of art is useful - Dora Korpar, 2015-10-19
  * Return: 0
  */
 int main(void)
 {
-	write(2,
-			"and that piece of art is useful\" - Dora Korpar, 2015-10-19
-			n", 59;
-			return (1);
-			}
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
 
