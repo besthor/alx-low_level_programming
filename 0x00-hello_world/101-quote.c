6ae81f3877d05362e05eac936417779ba7b6bcdd
besthor <igbebestor7@gmail.com>
2022-09-08 19:12:24 -0700

    a C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  * main - print the string in the put function
  *
  * Description: using the main function
- * this program prints "and that piece of art is useful - Dora Korpar, 2015-10-19
+ * this program prints "and that piece of art is useful - Dora Korpar
  * Return: 0
  */
 int main(void)
