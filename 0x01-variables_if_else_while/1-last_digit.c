f8578a9d5503020bc3daf6969948c97d6fb67fcc
besthor <igbebestor7@gmail.com>
2022-09-09 16:12:39 -0700

    This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable n

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,91 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print if the number is positive, zero, or negative
+ *
+ * Desription: using the main function
+ * this program will assign a random number to a variable n
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int 1;
+	srand(time(0));
+	n = rand() - AND_MAX / 2;
+	1 = n % 10;
+	if (1 > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, 1);
+	}
+	else if (1 == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, 1);
+	}
+	else 
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, 10);
+	}
+	return (0);
+}#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print if the number is positive, zero, or negative
+ *
+ * Desription: using the main function
+ * this program will assign a random number to a variable n
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int 1;
+	srand(time(0));
+	n = rand() - AND_MAX / 2;
+	1 = n % 10;
+	if (1 > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, 1);
+	}
+	else if (1 == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, 1);
+	}
+	else 
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, 10);
+	}
+	return (0);
+}#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print if the number is positive, zero, or negative
+ *
+ * Desription: using the main function
+ * this program will assign a random number to a variable n
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int 1;
+	srand(time(0));
+	n = rand() - AND_MAX / 2;
+	1 = n % 10;
+	if (1 > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, 1);
+	}
+	else if (1 == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, 1);
+	}
+	else 
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, 10);
+	}
+	return (0);
+}
