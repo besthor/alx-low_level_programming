e3f142c8afb1061cdf210d76c23210ea972ac2b7
besthor <igbebestor7@gmail.com>
2022-09-11 13:41:43 -0700

    a program that prints all possible combinations of two two-digit numbers.
    The numbers should range from 0 to 99
    
    The two numbers should be separated by a space
    
    All numbers should be printed with two digits. 1 should be printed as 01
    
    The combination of numbers must be separated by comma, followed by a space
    
    The combinations of numbers should be printed in ascending order

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,14 +13,13 @@ int main(void)
 
 	for (i = 0; i <= 99; i++)
 {
-	for(j = i + 1; j <=99; j++)
+	for (j = i + 1; j <= 99; j++)
 	{
 		putchar(i / 10 + '0');
 		putchar(i % 10 + '0');
 		putchar(' ');
 		putchar(j / 10 + '0');
 		putchar(j % 10 + '0');
-		
 		if (!(i == 98 && j == 99))
 		{
 			putchar(',');
