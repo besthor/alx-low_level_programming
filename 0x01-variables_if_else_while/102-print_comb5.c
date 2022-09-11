2892fdbaa4e39baf242d75150004d38da1c26b07
besthor <igbebestor7@gmail.com>
2022-09-11 13:32:20 -0700

    a program that prints all possible combinations of two two-digit numbers.
    The numbers should range from 0 to 99
    
    The two numbers should be separated by a space
    
    All numbers should be printed with two digits. 1 should be printed as 01
    
    The combination of numbers must be separated by comma, followed by a space
    
    The combinations of numbers should be printed in ascending order

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	for (i = 0; i <= 99; i++)
+{
+	for(j = i + 1; j <=99; j++)
+	{
+		putchar(i / 10 + '0');
+		putchar(i % 10 + '0');
+		putchar(' ');
+		putchar(j / 10 + '0');
+		putchar(j % 10 + '0');
+		
+		if (!(i == 98 && j == 99))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
