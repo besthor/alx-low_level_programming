555684f23490152c75d5d1047124d00a6cabd118
besthor <igbebestor7@gmail.com>
2022-09-12 04:55:48 -0700

    a program that prints all single digit numbers of base 10 starting from 0, followed by a new line

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print if the number is positive, zero, or negetive
+ * main - print if the number is positive, zero, or negative
  *
  * Description: using the main function
  * this program prints "Programming is positive, zero, or negative
@@ -8,11 +8,11 @@
  */
 int main(void)
 {
-int i,
-    for (i = 0; i < 10; i++)
-{
-	printf("%i", i);
-}
-printf("\n");
-return (0);
+	int n;
+	for (n = '0' ; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
 }
