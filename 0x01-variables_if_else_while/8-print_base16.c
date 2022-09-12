5a206a23617bea7c0caac5e57081b97a8dbe4e25
besthor <igbebestor7@gmail.com>
2022-09-12 06:18:04 -0700

    a program that prints all the numbers of base 16 in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print if the number is positive, zero, or negative
+ *
+ * Descrption: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0' ; c <= '9' ; c++)
+	{
+		putchar(c);
+	}
+	for
+		putchar(c);
+return (0);
+}
