afcb204403b8d512ff33b36761cbdd9ff266206f
besthor <igbebestor7@gmail.com>
2022-09-13 17:28:35 -0700

    A prototypes of all your functions and the prototype of the function _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ * Description: using the main function
+ * this prints _putchar followoed by a new line
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
