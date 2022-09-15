41c4400a7040c2f78fa14f1e08a435cd5ffa95d1
besthor <igbebestor7@gmail.com>
2022-09-15 08:08:07 -0700

    this main file is a test for a postitive_or_negative()

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,5 @@
+What is debugging
+
+What are some methods of debugging manually
+
+How to read the error messages
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+void print_remaining_days(int, int int);
+int convert_day(int, int);
+
+#endif /* MAIN_H */
