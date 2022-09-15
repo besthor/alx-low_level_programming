071804088ec67991fd9fc5d5bc2c10c3d7f677a3
besthor <igbebestor7@gmail.com>
2022-09-15 08:13:49 -0700

    this main file is a test for a postitive_or_negative()

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,13 @@
 #include "main.h"
-
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
-
 int main(void)
 {
-	int i:
+	int i;
 
-		i = 0;
+	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
