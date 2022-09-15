087d50c7fca4b855593130f937cab9ca4279c048
besthor <igbebestor7@gmail.com>
2022-09-15 08:37:01 -0700

    How to comment on an infinite loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main (void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	/**
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+
+	printf("Infinite loop avoided! \\0/\n");
+
+	return (0);
+}
