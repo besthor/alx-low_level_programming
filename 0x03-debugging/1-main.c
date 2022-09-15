54abf300a2b159a7b4a8ba9737821849443e4e7c
besthor <igbebestor7@gmail.com>
2022-09-15 08:59:36 -0700

    A program that prints Infinite loop avoided!

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,21 +5,20 @@
  * Return: 0
  */
 
-int main (void)
+int main(void)
 {
 	int i;
 
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-	/**
+	/*
 	 * while (i < 10)
 	 * {
 	 * putchar(i);
 	 * }
 	 */
-
-	printf("Infinite loop avoided! \\0/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
