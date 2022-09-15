4d0c7c391ee14ce2166484ee604792a8679150ce
besthor <igbebestor7@gmail.com>
2022-09-15 11:39:15 -0700

    This program prints the largest of three integers

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int, int, int);
-void print_remaining_days(int, int int);
-int convert_day(int, int);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif /* MAIN_H */
