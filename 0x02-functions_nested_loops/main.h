f37cba4874e506500b7e1f426500046ba17aab74
besthor <igbebestor7@gmail.com>
2022-09-16 05:11:52 -0700

    a function that prints the n times table, starting with 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,6 +13,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
-void print_times_table(int n);
+void print_times_table(int n);					
 #endif
