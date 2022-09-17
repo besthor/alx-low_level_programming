114a15e804b586047ca9fe8a6fbf2689d8ed63f7
besthor <igbebestor7@gmail.com>
2022-09-17 02:47:31 -0700

    function that prints an integer.
    Prototype: void print_number(int n);
    
    You can only use _putchar function to print
    
    You are not allowed to use long
    
    You are not allowed to use arrays or pointers
    
    You are not allowed to hard-code special values

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,6 +8,7 @@
 void print_number(int n)
 {
 	char ld, cr;
+	int r;
 	int ct = 0;
 
 	if (n < 0)
@@ -16,6 +17,32 @@ void print_number(int n)
 		ld = (char)('0' - (n % 10));
 		n = n / -10;
 	}
+
 	else
 	{
 		ld = (char)((n % 10) + '0');
+		n = n / 10;
+	}
+
+	r = 0;
+	while (n > 0)
+	{
+		r = r * 10 + (n % 10);
+		n = n / 10;
+		ct++;
+	}
+
+	while (r > 0)
+	{
+		cr = (char)((r % 10) + '0');
+		_putchar(cr);
+		r = r / 10;
+		ct--;
+	}
+	while (ct != 0)
+	{
+		_putchar('0');
+		ct--;
+	}
+	_putchar(ld);
+}
