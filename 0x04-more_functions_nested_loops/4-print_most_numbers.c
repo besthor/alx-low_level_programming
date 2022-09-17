1826b42b6fa99150e6ad9f313d62e56456aed2aa
besthor <igbebestor7@gmail.com>
2022-09-16 21:14:10 -0700

    a function that prints the numbers, from 0 to 9, followed by a new line.
    Prototype: void print_most_numbers(void);
    
    Do not print 2 and 4
    
    You can only use _putchar twice in your code

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,16 +3,17 @@
 
 /**
  * print_most_numbers - prints numbers except 2 and 4
+ *
  * Return: returns nothing
  */
 
 void print_most_numbers(void)
 {
 	int n;
-
+	
 	for (n = 48; n < 58; n++)
 	{
-		if ((n == 50)  || (n == 520))
+		if ((n == 50)  || (n == 52))
 		{
 			continue;
 		}
