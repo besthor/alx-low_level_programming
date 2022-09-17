48af433cd767e6b0f14d366cc7682b86add67208
besthor <igbebestor7@gmail.com>
2022-09-16 20:22:08 -0700

    a function that checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,13 @@
+What are nested loops and how to use them
+
+What is a function and how do you use functions
+
+What is the difference between a declaration and a definition of a function
+
+What is a prototype
+
+Scope of variables
+
+What are the gcc flags -Wall -Werror -pedantic -Wextra -std=gnu89
+
+What are header files and how to to use them with #include
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header file for prototypes
+ * void prototypes (void)
+ * int prototypes (int)
+ */
+
+int_putchar(char);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif /* MAIN_H */
+
