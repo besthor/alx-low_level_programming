74903b83100b016ad8915b7e0102364eb4cdb169
besthor <igbebestor7@gmail.com>
2022-09-19 15:35:10 -0700

    a function that convert a string to an integer.
    Prototype: int _atoi(char *s);
    
    The number in the string can be preceded by an infinite number of characters
    
    You need to take into account all the - and + signs before the number
    
    If there are no numbers in the string, the function must return 0
    
    You are not allowed to use long
    
    You are not allowed to declare new variables of “type” array
    
    You are not allowed to hard-code special values
    
    We will use the -fsanitize=signed-integer-overflow gcc flag to compile your code.

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generates keygen.
+ * Return: return 0 always
+ */
+
+int main(void)
+{
+	int r = 0, c = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+	while (c < 2772)
+	{
+		r = rand() % 128;
+		if ((c + r) > 2772)
+			break;
+		c = c + r;
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - c ));
+	return (0);
+}
