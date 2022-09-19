713d4073eee6ef4fefd98391875f41ef0258c7a9
besthor <igbebestor7@gmail.com>
2022-09-19 16:07:09 -0700

    a function that convert a string to an integer.
    Prototype: int _atoi(char *s);
    
    The number in the string can be preceded by an infinite number of characters
    
    You need to take into account all the - and + signs before the number
    
    If there are no numbers in the string, the function must return 0
    
    You are not allowed to use long
    
    You are not allowed to declare new variables of “type” array
    
    You are not allowed to hard-code special values
    
    We will use the -fsanitize=signed-integer-overflow gcc flag to compile your code

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,46 +4,41 @@
 
 /**
  * _atoi - This function converts a string to an integer.
- * @s: Pointer parameter
- * Return: returns an integer value
+ * @s: char array string
+ * Description: Number in the string can be preceded by an infinite
+ * number of characters.
+ * You need to take into account all -/+ signs before the number.
+ * If there are no numbers in the string, return 0.
+ * No need to check for overflow.
+ * Not allowed to hard-code special values.
+ * Return: first integer found in string
  */
 
 int _atoi(char *s)
 {
 	int i;
-	int np = 0;
-	int c;
-	int d = 1;
-	int num = 0;
+	int h, p;
 
-	for (i = 0; i < _strlen(s); i++)
+	h = 0;
+	p = -1;
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (!(s[i] >= '0' && s[i] <= '9') && c > 0)
-			break;
 		if (s[i] == '-')
-			np--;
-		if (s[i] == '+')
-			np++;
-		if (s[i] >= '0' && s[i] <= '9')
+			p *= -1;
+
+		if (s[i] > 47 && s[i] < 58)
 		{
-			c++;
+			if (h < 0)
+				h = (h * 10) - (s[i] - '0');
+			else
+				h = (s[i] - '0') * -1;
+
+			if (s[i + 1] < 48  ||  s[i + 1] > 57)
+				break;
 		}
 	}
-	while (c > 0)
-	{
-		num += ((s[i - 1] - '0') * d);
-		i--;
-		c--;
-		d *= 10;
-	}
-	if (np >= 0)
-	{
-		num *= 1;
-	}
-	else
-	{
-		num *= -1;
-	}
-	return (num);
-}
+	if (p < 0)
+		h *= -1;
 
+	return (h);
+}
