a0fe1b9d6a36e52bf6c4a3302361c59079b89445
besthor <igbebestor7@gmail.com>
2022-09-21 15:08:40 -0700

    a function that copies a string.
    Prototype: char *_strncpy(char *dest, char *src, int n);
    
    Your function should work exactly like strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,14 +11,9 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-
 	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
 		dest[i] = scr[i];
-	}
-	for (; n > i; i++)
-	{
+	for (; i < n; i++)
 		dest[i] = '\0';
-	}
 	return (dest);
 }
