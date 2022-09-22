6c6866d61974f11e2a26d05a3b79de5119bb7399
besthor <igbebestor7@gmail.com>
2022-09-22 07:49:35 -0700

    a function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,22 +3,25 @@
 
 /**
  * rot13 - rotate characters 13 places in the alphabet
- * @s: string
- * Return: atring 's' rotated
+ * @s: string to encrypt
+ * Return: string 's' rotated
  */
 
 char *rot13(char *s)
 {
-	int i;
-	char storeh[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-	char storel[] = "nopqrstuvwxyzabcdefghijklm";
+	int i, j;
+	char a[] = "ABCDEFGHIJLLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIKJLMopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if ((s[i] > 64 && s[i] < 91) || (s[i] > 96 && s[i] < 123))
+		for (j = 0; a[j] != '\0'; j++)
 		{
-			s[i] = (s[i] - 65 > 25)
-				storel[s[i] - 97] : storeh[s[i] - 65];
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
 		}
 	}
 	return (s);
