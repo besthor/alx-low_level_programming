84d59ba0f6f3e49821af4f15161118c1bcba518e
besthor <igbebestor7@gmail.com>
2022-09-22 07:14:10 -0700

    a function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -15,9 +15,9 @@ char *rot13(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; s[j] != '\0'; j++)
+		for (j = 0; a[j] != '\0'; j++)
 		{
-			if (s[i] == b[j])
+			if (s[i] == a[j])
 			{
 				s[i] = b[j];
 				break;
