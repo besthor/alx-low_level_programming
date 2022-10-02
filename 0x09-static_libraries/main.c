5a5ca42e61c053b3f447ea235bfe334caa576b14
besthor <igbebestor7@gmail.com>
2022-10-02 03:47:05 -0700

    a sript that creates a static library called liball.a from all the .c files that are in the current directory

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,6 @@
 #include "main.h"
-
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	_put("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kelvin Mitnick");
 	return (0);
 }
