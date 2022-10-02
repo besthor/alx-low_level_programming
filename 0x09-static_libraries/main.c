7b71c85496556a4d09f0d5d515fcf3537b2662d1
besthor <igbebestor7@gmail.com>
2022-10-02 03:52:47 -0700

    a script that that creates a static library called liball.a from all the .c files that are in the current directory

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 int main(void)
 {
-	_put("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kelvin Mitnick");
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kelvin Mitnick");
 	return (0);
 }
