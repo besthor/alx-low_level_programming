64de2d00e6b87f5206acb1fd97a878c16db7a478
besthor <igbebestor7@gmail.com>
2022-10-04 06:00:51 -0700

    a function that creates an array of chars, and initializes it with a specific char.

---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,9 @@
+General
+
+What is the difference between automatic and dynamic allocation
+
+What is malloc and free and how to use them
+
+Why and when use malloc
+
+How to use valgrind to check for memory leak
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+
+int _putchar(char);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+
+#endif /* MAIN_H */
