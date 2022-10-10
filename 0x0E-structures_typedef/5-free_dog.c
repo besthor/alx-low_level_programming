022656e8036bebc3f85367aa53410d2251c7d8e8
besthor <igbebestor7@gmail.com>
2022-10-10 15:08:33 -0700

    a function that prints a struct dog,

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <sdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - free memory of dog
+ * @d: poiner to dog
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
+		free(d);
+	}
+}
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,5 @@
+General
+
+What are structures, when, why and how to use them
+
+How to use typedef
