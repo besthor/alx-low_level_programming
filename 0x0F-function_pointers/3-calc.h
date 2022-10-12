bd851ef4a826f479d2cf88bd54b1a710c7c69b87
besthor <igbebestor7@gmail.com>
2022-10-12 10:41:21 -0700

    a file that contains all the function prototypes and data structures used by the program

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -15,7 +15,7 @@ typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-}op_t;
+} op_t;
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -29,5 +29,4 @@ int (*get_op_func(char *s))(int, int)
 		i++;
 	}
 
-	return (NULL);
-}
+	return 
