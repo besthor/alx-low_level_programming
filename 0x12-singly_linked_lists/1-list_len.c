59b99f84e65f985220d67a05681dd5f9398c58ed
besthor <igbebestor7@gmail.com>
2022-10-20 11:49:08 -0700

    a function that returns the number of elements in a linked list_t list.

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -4,7 +4,7 @@
  * list_len - returns the number of elements in a linked lists
  * @h: linked list of type listint_t to traverse
  *
- * Return: number of nodes
+ * Return: number of elements in a linked list_t list.
  */
 size_t list_len(const list_t *h)
 {
