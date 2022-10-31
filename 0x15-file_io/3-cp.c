6f15603a4b6b2f14657a47c6ef4370f96ca35a72
besthor <igbebestor7@gmail.com>
2022-10-31 15:18:31 -0700

    a program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -44,8 +44,6 @@ int main(int argc, char *argv[])
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
 
-
-
 	nchars = 1024;
 	while (nchars == 1024)
 	{
