bd1dee276ac70d642840c4a9173c92e31677d7b4
Besthor <111004790+besthor@users.noreply.github.com>
2023-02-24 12:47:39 +0100

    Update 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - print 1 to 100, except certain multiples
+ * multiple of 3 only displays "Fizz" instad of number.
+ * multiple of 5 displays "Buzz" instead of number.
+ * multiples of 3 and 5 display "FizzBuzz"
+ * Return: 0 on Success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+
+	}
+	printf("\n");
+	return (0);
+}
