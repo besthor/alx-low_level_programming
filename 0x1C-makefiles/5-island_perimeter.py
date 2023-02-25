b00396ed52cd116511ed690409d3336de2ae263a
besthor <igbebestor7@gmail.com>
2023-02-24 19:42:24 -0800

    Island Perimeter: Technical interview preparation

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,45 +1,26 @@
 #!/usr/bin/python3
+"""Defines an island perimeter measuring function."""
+
 
 def island_perimeter(grid):
-    """
-    Calculates the perimeter of an island described in the given grid.
-
+    """Return the perimiter of an island.
+    The grid represents water by 0 and land by 1.
     Args:
-        grid: A list of lists of integers representing the island. 0 represents a water zone, 1 represents a land zone.
-
+        grid (list): A list of list of integers representing an island.
     Returns:
-        An integer representing the perimeter of the island.
-
-    Raises:
-        ValueError: If the grid is not rectangular, or if the island has lakes (water inside that isn't connected to the water around the island).
+        The perimeter of the island defined in grid.
     """
-    rows = len(grid)
-    cols = len(grid[0])
-    perimeter = 0
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-    for i in range(rows):
-        for j in range(cols):
+    for i in range(height):
+        for j in range(width):
             if grid[i][j] == 1:
-                # check left
-                if j == 0 or grid[i][j-1] == 0:
-                    perimeter += 1
-                # check right
-                if j == cols-1 or grid[i][j+1] == 0:
-                    perimeter += 1
-                # check top
-                if i == 0 or grid[i-1][j] == 0:
-                    perimeter += 1
-                # check bottom
-                if i == rows-1 or grid[i+1][j] == 0:
-                    perimeter += 1
-
-    # check for lakes
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                if i > 0 and grid[i-1][j] == 0 and (j == 0 or grid[i][j-1] == 0) and (j == cols-1 or grid[i][j+1] == 0):
-                    raise ValueError("Grid has a lake")
-                if j > 0 and grid[i][j-1] == 0 and (i == 0 or grid[i-1][j] == 0) and (i == rows-1 or grid[i+1][j] == 0):
-                    raise ValueError("Grid has a lake")
-
-    return perimeter
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
