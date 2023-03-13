d61a3a314583a30ad3fcb0a96c7fcd819750d6b3
Besthor <111004790+besthor@users.noreply.github.com>
2023-03-13 20:28:48 +0100

    Update 2-str_concat-1.c

--- 0x0B-malloc_free/2-str_concat-1.c
+++ 0x0B-malloc_free/2-str_concat-1.c
@@ -1,3 +1,13 @@
+#include "main.h"
+
+/**
+ * str_concat - a function that concatenates two strings.
+ * @s1:First string
+ * @s2:Second string
+ *
+ * Return: NULL in case of failure , but pointer to new string in
+ * case of success
+ */
 char *str_concat(char *s1, char *s2) {
     if (s1 == NULL || s2 == NULL) {
         return NULL;
