abab366cc88688cf561bcc256d3c24a7f8329577
Besthor <111004790+besthor@users.noreply.github.com>
2023-03-20 05:26:23 +0100

    A program that prints the alphabet in lowercase, followed by a new line

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
+
 /**
- * main - print if the number is positive, zero, or negative
+ * main - Entry point
  *
- * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
- * Return: 0
+ * Description: This program prints the lowercase alphabet,
+ * excluding the letters 'e' and 'q'.
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-if (ch != 'e' && ch != 'q')
-{
-	putchar(ch);
-}
-}
-putchar('\n');
-return (0);
+    char ch;
+
+    for (ch = 'a'; ch <= 'z'; ch++)
+    {
+        if (ch != 'e' && ch != 'q')
+        {
+            putchar(ch);
+        }
+    }
+    putchar('\n');
+    return (0);
 }
